bafee3d49bcd653f51f0985d7e1eadb89fe0eb2e
Volodymyr Huk <nailz2kua@gmail.com>
2022-06-25 01:53:17 +0300

    add ant

---
+++ theme-generator.js
@@ -0,0 +1,22 @@
+const less = require('less');
+const LessPluginCleanCSS = require('less-plugin-clean-css');
+const fs = require('fs');
+
+// ng zorro defined styles
+const basicStyles = `@import './node_modules/ng-zorro-antd/ng-zorro-antd.less';`;
+// ng zorro compact theme variables
+
+less.render(`${basicStyles}`, {
+    javascriptEnabled: true,
+    plugins: [new LessPluginCleanCSS({ advanced: true })],
+    modifyVars: {
+        'primary-color': '#48C9B0',
+    }
+}).then(data => {
+    fs.writeFileSync(
+        './src/styles/ant-theme.css',
+        data.css
+    )
+}).catch(e => {
+    console.error(e);
+});
